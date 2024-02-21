fe841dcdaa0b312f74f7a271ba6ecd5bb416316a
unknown <maindadpranita@gmail.com>
2024-02-21 23:13:18 +0530

    Dashboard addded

--- typing/src/App.js
+++ typing/src/App.js
@@ -1,12 +1,19 @@
 
 import Dashboard from './Components/Dashboard';
+import Login1 from './Components/Login1';
 import TypingTest from './Components/Practice';
-
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-   <Dashboard></Dashboard>
+   {/* <Dashboard></Dashboard> */}
    {/* <TypingTest></TypingTest> */}
+   <Routes>
+    <Route path='/' Component={Login1}></Route> 
+    <Route path='/dashboard' Component={Dashboard}></Route>
+    
+   </Routes>
+   {/* <Login1/> */}
     </div>
   );
 }
